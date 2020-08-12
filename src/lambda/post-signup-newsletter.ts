6f2389dd26e43825da7c250cfce76f1beaecedb9
Dorell James <galangdj@gmail.com>
2020-08-12 23:52:56 +0800

    Newsletter signup via Netlify functions

--- src/lambda/post-signup-newsletter.ts
+++ src/lambda/post-signup-newsletter.ts
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export async function handler(event, context) {
   if (event.httpMethod !== "POST") {
     return {
@@ -6,10 +8,52 @@ export async function handler(event, context) {
     }
   }
 
-  // @todo: Do the actual implementation here..
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Successfully signup to newsletter!" }),
+  const { email } = JSON.parse(event.body)
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Email is required!" }),
+    }
   }
+
+  const {
+    MAILCHIMP_API_KEY,
+    MAILCHIMP_LIST_ID,
+    MAILCHIMP_SUBDOMAIN,
+  } = process.env
+
+  return axios
+    .post(
+      `https://${MAILCHIMP_SUBDOMAIN}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`,
+      { email_address: email, status: "pending" },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${Buffer.from(
+            `apikey:${MAILCHIMP_API_KEY}`
+          ).toString("base64")}`,
+        },
+      }
+    )
+    .then(result => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Successfully subscribed to newsletter!",
+        }),
+      }
+    })
+    .catch(err => {
+      if (err.response) {
+        return {
+          statusCode: err.response.status,
+          body: JSON.stringify(err.response.data),
+        }
+      }
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err && err.message),
+      }
+    })
 }
