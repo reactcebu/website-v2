0e166fddafe71237788963b4f0aad5d14ec822e7
Cleo Credo <credocleo28@gmail.com>
2020-10-21 22:43:01 +0800

    added content for contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,20 +1,46 @@
 import { Layout } from "../components"
 import React from "react"
+import styled from "styled-components"
 
 const ContactPage = () => {
   return (
     <Layout>
       <div
         style={{
-          display: "flex",
+          // display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <h2>Contact page coming soon...</h2>
+        <Section>
+          <h2>We'd love to hear from you!</h2>
+          <h3>Drop us a line</h3>
+        </Section>
+
+        <Section>
+          <p>Email: react@jscebu.org</p>
+          <p>Facebook Page: https://www.facebook.com/reactcebu</p>
+        </Section>
       </div>
     </Layout>
   )
 }
+
+const Section = styled.section`
+  display: block;
+  max-width: 1440px;
+  // border: 1px solid red;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 28px;
+  padding: 40px 30px;
+
+  h2 {
+    font-size: 60px;
+    max-width: 900px;
+    margin: 0 auto;
+    margin-bottom: 18px;
+  }
+`
 
 export default ContactPage
