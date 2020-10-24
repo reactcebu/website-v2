c30acc51c0cd810431c4462349dc8e69cd2c33c4
Dorell James <galangdj@gmail.com>
2020-10-24 16:13:09 +0800

    Improvements on styling. Added a few more things

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -14,12 +14,31 @@ const ContactPage = () => {
       >
         <Section>
           <h2>We'd love to hear from you!</h2>
-          <h3>Drop us a line</h3>
         </Section>
 
         <Section>
-          <p>Email: react@jscebu.org</p>
-          <p>Facebook Page: https://www.facebook.com/reactcebu</p>
+          <p>
+            If you have any questions, suggestions, or whatsoever, please email
+            us at{" "}
+            <a href={"mailto: react@jscebu.org"} target="_blank">
+              react@jscebu.org
+            </a>{" "}
+            or you could also leave us a message at our{" "}
+            <a href="https://facebook.com/reactcebu" target="_blank">
+              Facebook page
+            </a>
+            .
+          </p>
+
+          <p>
+            We also have a messenger group chat named{" "}
+            <strong>Reactions v2</strong> where all Reactors are chilling and
+            hanging out. If you'd like to join,{" "}
+            <a href="https://m.me/reactcebu" target="_blank">
+              please leave us a message
+            </a>{" "}
+            and we'll invite you.
+          </p>
         </Section>
       </div>
     </Layout>
@@ -40,6 +59,11 @@ const Section = styled.section`
     max-width: 900px;
     margin: 0 auto;
     margin-bottom: 18px;
+  }
+
+  p {
+    text-align: left;
+    line-height: 150%;
   }
 `
 
