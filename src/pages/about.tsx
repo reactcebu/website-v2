12b5aba5b5010891283396cdb71bf23882c5318a
Dorell James <galangdj@gmail.com>
2020-10-24 15:37:16 +0800

    Improve readability of About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,35 +5,39 @@ import styled from "styled-components"
 const AboutPage = () => {
   return (
     <Layout>
-      <div
-        style={{
-          // display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Section>
-          <h2>React Cebu — a community for developers by developers</h2>
-        </Section>
+      <Section>
+        <h2>React Cebu — a community for developers by developers</h2>
+        <br />
+        <br />
 
-        <Section>
-          <p>
-            We, React Cebu, aims to spread the usage of ReactJS and other
-            JavaScript technologies to the community by organizing free meetups,
-            workshops, code camps, trainings and more. Through these, we hope to
-            create a friendly, inclusive, and positive space for our community
-            members to engage and excel in this area. This is also to uplift
-            other developers through free learning, knowledge sharing and
-            collaboration. Ultimately, we hope to help in fostering cutting edge
-            developers in Cebu City. We believe in the importance of finding
-            your tribe — the people who'll be your cheerleaders, you'll grow and
-            learn with — and that's what React Cebu hopes to be. Everyone is
-            welcome to join this community by developers, for developers. We
-            appreciate more hands! Let us know if you have the desire to help us
-            bring more developer events. Send us a message at react@jscebu.org
-          </p>
-        </Section>
-      </div>
+        <p>
+          We, <strong>React Cebu</strong>, aims to spread the usage of React JS
+          and other JavaScript technologies to the community by organizing free
+          meetups, workshops, code camps, trainings and more. Through these, we
+          hope to create a friendly, inclusive, and positive space for our
+          community members to engage and excel in this area. This is also to
+          uplift other developers through free learning, knowledge sharing and
+          collaboration. Ultimately, we hope to help in fostering cutting edge
+          developers in Cebu City.
+        </p>
+
+        <p>
+          We believe in the importance of finding your tribe — the people who'll
+          be your cheerleaders, you'll grow and learn with — and that's what
+          React Cebu hopes to be.
+        </p>
+
+        <p>
+          Everyone is welcome to join this{" "}
+          <em>community by developers, for developers.</em>
+        </p>
+
+        <p>
+          We appreciate more hands! Let us know if you have the desire to help
+          us bring more developer events. Send us a message at{" "}
+          <a href={"mailto: react@jscebu.org"}>react@jscebu.org</a>
+        </p>
+      </Section>
     </Layout>
   )
 }
@@ -52,6 +56,11 @@ const Section = styled.section`
     max-width: 900px;
     margin: 0 auto;
     margin-bottom: 18px;
+  }
+
+  p {
+    line-height: 150%;
+    text-align: left;
   }
 `
 
