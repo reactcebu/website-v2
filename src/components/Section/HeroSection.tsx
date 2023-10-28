8957ded164fff26193e7f2d6118ac8fa59d6b084
20100215 <20100215@usc.edu.ph>
2023-10-28 16:48:03 +0800

    [Style] Added padding to layout container

--- src/components/Section/HeroSection.tsx
+++ src/components/Section/HeroSection.tsx
@@ -1,13 +1,4 @@
 import { Section } from "./Section"
 import styled from "styled-components"
 
-export const HeroSection = styled(Section)`
-  padding: 80px 0;
-
-  h1 {
-    font-size: 72px;
-    max-width: 900px;
-    margin: 0 auto;
-    margin-bottom: 18px;
-  }
-`
+export const HeroSection = styled(Section)``
